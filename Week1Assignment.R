405b15d087a98b668240fdf8b1a85fd88cadda58
kishkp <kishkp@yahoo.com>
2015-10-30 20:49:33 +0400

    Week 1 Assignment
    Initial Version

---
+++ Week1Assignment.R
@@ -0,0 +1,29 @@
+
+# R Bridge - Week 1 Assignment
+
+# 1. Write a loop that calculates 12-factorial.
+        Fact12<-1
+        
+        for (i in 1:12) {
+                Fact12 <- Fact12 * i 
+        }
+        
+        sprintf("The Factorial for 12 is : %s", Fact12 )
+
+
+# 2. Show how to create a numeric vector that contains the sequence from 20 to 50 by 5.
+        numvect<-seq(20,50, by=5)
+
+
+# 3. Show how to take a trio of input numbers a, b, and c and implement the quadratic equation.
+        QuadEqString<-function(a, b, c){
+                sprintf("%sx2 + %sx + %s", a, b, c)
+        }
+
+        eq1<-QuadEqString(10,5,3)
+        eq1
+        
+        
+        
+        
+        
